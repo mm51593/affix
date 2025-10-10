18a61cf1d7079504cb4d0e8a66527cf50fe6884d
Maksimilijan Marošević <maksimilijan.marosevic@proton.me>
2025-10-10 22:54:45 +0200

    Convert Flags protocol into FlagsBase macro

--- lib/flags/cc_flags.ex
+++ lib/flags/cc_flags.ex
@@ -1,4 +1,6 @@
 defmodule CcFlags do
+  use FlagsBase
+
   defstruct [
     :flag_include,
     :flag_output,
@@ -9,32 +11,8 @@ defmodule CcFlags do
   def get_switch(:flag_output), do: "-o"
   def get_switch(:flag_no_link?), do: "-c"
 
-  def new(flags_map) do
-    flags_map
-    |> Map.new(fn {k, v} -> {k, %Flag{key: k, value: v}} end)
-    |> then(&struct(__MODULE__, &1))
-  end
-
   def get_flags(cc_flags) do
     vals = cc_flags |> Map.from_struct() |> Map.values()
     Enum.filter(vals, &is_struct(&1, Flag))
-  end
-
-  def render_flag(%Flag{value: nil}), do: []
-  def render_flag(%Flag{value: false}), do: []
-  def render_flag(%Flag{value: ""}), do: []
-  def render_flag(%Flag{value: true, key: key}), do: [__MODULE__.get_switch(key)]
-
-  def render_flag(%Flag{value: value, key: key}) when is_list(value),
-    do: Enum.flat_map(value, fn x -> [__MODULE__.get_switch(key), x] end)
-
-  def render_flag(%Flag{value: value, key: key}),
-    do: [__MODULE__.get_switch(key), value]
-end
-
-defimpl Flags, for: CcFlags do
-  def render(cc_flags) do
-    flags = CcFlags.get_flags(cc_flags)
-    Enum.flat_map(flags, &CcFlags.render_flag(&1))
   end
 end
--- lib/flags/cc_linker_flags.ex
+++ lib/flags/cc_linker_flags.ex
@@ -1,4 +1,6 @@
 defmodule CcLinkerFlags do
+  use FlagsBase
+
   defstruct [
     :flag_output,
     :flag_libs
@@ -7,32 +9,8 @@ defmodule CcLinkerFlags do
   def get_switch(:flag_output), do: "-o"
   def get_switch(:flag_libs), do: "-l"
 
-  def new(flags_map) do
-    flags_map
-    |> Map.new(fn {k, v} -> {k, %Flag{key: k, value: v}} end)
-    |> then(&struct(__MODULE__, &1))
-  end
-
   def get_flags(cc_linker_flags) do
     vals = cc_linker_flags |> Map.from_struct() |> Map.values()
     Enum.filter(vals, &is_struct(&1, Flag))
-  end
-
-  def render_flag(%{value: nil}), do: []
-  def render_flag(%{value: false}), do: []
-  def render_flag(%{value: ""}), do: []
-  def render_flag(%{value: true, key: key}), do: [__MODULE__.get_switch(key)]
-
-  def render_flag(%{value: value, key: key}) when is_list(value),
-    do: Enum.flat_map(value, fn x -> [__MODULE__.get_switch(key), x] end)
-
-  def render_flag(%{value: value, key: key}),
-    do: [__MODULE__.get_switch(key), value]
-end
-
-defimpl Flags, for: CcLinkerFlags do
-  def render(cc_linker_flags) do
-    flags = CcLinkerFlags.get_flags(cc_linker_flags)
-    Enum.flat_map(flags, &CcLinkerFlags.render_flag(&1))
   end
 end
