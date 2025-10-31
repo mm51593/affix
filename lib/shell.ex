50a9ad8aefea9ea002acb714a48297de44aa719c
Maksimilijan Marošević <maksimilijan.marosevic@proton.me>
2025-10-31 23:53:13 +0100

    Shell: add exec with split argument types

--- lib/shell.ex
+++ lib/shell.ex
@@ -1,10 +1,14 @@
 defmodule Shell do
-  def exec(cmd, args) do
-    {output, exit_code} = System.cmd(cmd, args)
-    IO.puts("cmd: #{cmd}:")
+  def exec(exe, args) do
+    {output, exit_code} = System.cmd(exe, args)
+    IO.puts("exe: #{exe}:")
     IO.inspect(args)
     IO.puts("output:\n#{output}")
     IO.puts("exit code: #{exit_code}")
     {output, exit_code}
+  end
+
+  def exec(exe, cmd, flags, positional) do
+    exec(exe, cmd ++ flags ++ positional)
   end
 end
