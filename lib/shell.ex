07e43608b588f57193c89fb48e415ce58e9f4271
Maksimilijan Marošević <maksimilijan.marosevic@proton.me>
2025-11-01 01:29:27 +0100

    Revert "Shell: add exec with split argument types"
    This reverts commit 50a9ad8aefea9ea002acb714a48297de44aa719c.

--- lib/shell.ex
+++ lib/shell.ex
@@ -1,14 +1,10 @@
 defmodule Shell do
-  def exec(exe, args) do
-    {output, exit_code} = System.cmd(exe, args)
-    IO.puts("exe: #{exe}:")
+  def exec(cmd, args) do
+    {output, exit_code} = System.cmd(cmd, args)
+    IO.puts("cmd: #{cmd}:")
     IO.inspect(args)
     IO.puts("output:\n#{output}")
     IO.puts("exit code: #{exit_code}")
     {output, exit_code}
-  end
-
-  def exec(exe, cmd, flags, positional) do
-    exec(exe, cmd ++ flags ++ positional)
   end
 end
